5567e665a391ee99b20c55d0cac367987a1a097f
Alexander Lougovski <a.lougovski@gmail.com>
2022-06-08 20:46:02 +0200

    trying to fix corruption problem with mongodb

--- omadacontroller/backup.sh
+++ omadacontroller/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# we need to stop db to avoid corruption
+mongod --dbpath /opt/tplink/EAPController/data/db --shutdown
 for d in data work logs
 do
    cp -a /opt/tplink/EAPController/$d/. /config/omada/$d/
