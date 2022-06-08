e6b994d044cd5e3c3a77cd042102c80570301bf4
Alexander Lougovski <a.lougovski@gmail.com>
2022-06-08 20:27:57 +0200

    patching to fix db

--- omadacontroller/entrypoint_wrapper.sh
+++ omadacontroller/entrypoint_wrapper.sh
@@ -6,4 +6,8 @@ do
    cp -a /config/omada/$d/. /opt/tplink/EAPController/$d/
 done
 
+#first fixing db????
+rm -f -- /opt/tplink/EAPController/data/db/mongod.lock
+mongod --dbpath /opt/tplink/EAPController/data/db --repair
+
 source /entrypoint_orig.sh
