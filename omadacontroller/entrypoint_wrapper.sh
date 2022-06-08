9169877a127adc7ffc963c6380b766d9f1418e86
Alexander Lougovski <a.lougovski@gmail.com>
2022-06-08 21:59:21 +0200

    Trying signal handling to backup config

--- omadacontroller/entrypoint_wrapper.sh
+++ omadacontroller/entrypoint_wrapper.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -x
 
 for d in data work logs
 do
@@ -6,6 +7,28 @@ do
    cp -a /config/omada/$d/. /opt/tplink/EAPController/$d/
 done
 
-source /entrypoint_orig.sh
+pid=0
 
-/backup.sh
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+    /backup.sh
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+# run application
+/entrypoint_orig.sh
+pid="$!"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
